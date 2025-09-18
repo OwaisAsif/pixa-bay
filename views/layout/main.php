b99ade02a75ec6f979e5630482b7424b50b3a807
ammaz-awan <ammazawan@gmail.com>
2025-09-18 06:22:26 -0700

    changes

---
+++ views/layout/main.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixabay</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <link rel="stylesheet" href="../../Assets/css/main.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent position-fixed w-100 p-0">
+        <div class="container-fluid">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="navbar-brand " href="#">
+                            <h2><span>Pixabay</span></h2>
+                        </a>
+                    </li>
+
+                </ul>
+
+                <button class="btn-1 h-50 w-60 py-2 px-3" type="login">Login</button>
+                <button class="btn-2 mx-4 h-50 w-70 py-2 px-3" type="join">Join</button>
+                <button class="btn-3 mr-5 h-50 w-60 py-2 px-3" type="upload">Upload</button>
+
+            </div>
+        </div>
+    </nav>
+
+    <!--Navbar Ends-->
+
+    <section class="header-section">
+        <div class="container-2">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="h">Stunning royalty-free images & royalty-free stock</h1>
+                </div>
+                <div class="col-12">
+                    <p class="p">Over 4.9 million+ high quality stock images,videos and music shared by our talented
+                        community.</p>
+                </div>
+                <div class="col-12">
+                    <input type="search " class="input" placeholder="Search for all images on pixabay">
+                </div>
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </section>
+
+    <section>
+        <div class="mx-5 px-5 d-flex justify-content-center py-5 " style="gap: 40px;">
+            <button type="button" class="btn">Home</button>
+            <button type="button" class="btn">Photos</button>
+            <button type="button" class="btn">Illistration</button>
+            <button type="button" class="btn">Vectors</button>
+            <button type="button" class="btn">Videos</button>
+            <button type="button" class="btn">Music</button>
+            <button type="button" class="btn">Sound Effects</button>
+            <button type="button" class="btn">GIFs</button>
+
+        </div>
+    </section>
+
+    <section>
+        <div class="mx-5 px-5 d-flex justify-content-center py-5 mb-4" style="gap: 10px;">
+            <button class="sr-only px-3">background</button>
+            <button class="sr-only px-3">wallpaper</button>
+            <button class="sr-only px-3">flowers</button>
+            <button class="sr-only px-3">woman</button>
+            <button class="sr-only px-3">landscape</button>
+            <button class="sr-only px-3">people</button>
+            <button class="sr-only px-3">money</button>
+            <button class="sr-only px-3">sea</button>
+            <button class="sr-only px-3">travel</button>
+            <button class="sr-only px-3">house</button>
+            <button class="sr-only px-3">iphone</button>
+            <button class="sr-only px-3">flags</button>
+        </div>
+    </section>
+
+    <section style="padding: 20px;">
+        <div class="row">
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12"><img src="../../Assets/pics/pics/2/donkey-8575524_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/1/basket-to-the-sea-8449952_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/3/lion-8947711_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/1/blue-tit-8432158_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/1/businessman-1477601_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/mountains-7543273_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/pasta-8865344_1280.jpg" alt=""></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12"><img src="../../Assets/pics/pics/2/cabin-8579310_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/2/concert-hall-8593024_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/2/horse-8414411_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/1/austria-6934186_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/2/dog-8946829_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/bird-8047149_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/bird-8936789_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/yoga-8843808_1280.jpg" alt=""></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12"><img src="../../Assets/pics/pics/3/leaves-8962041_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/1/bird-8785666_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/3/monkeys-8583435_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/3/mountains-6818253_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/3/rain-8866774_1280.png" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/girl-7832385_1280.jpg" alt=""></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12"><img src="../../Assets/pics/pics/4/seagull-8512995_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/4/ship-8170663_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/4/sunrise-8188929_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/4/tiger-3198598_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/4/woman-8937604_1280.png" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/businessman-7861357_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/sugar-blader-8265868_1280.jpg" alt=""></div>
+                    <div class="col-12"><img src="../../Assets/pics/pics/swing-8872109_1280.jpg" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class='px-5 py-4 border mb-4'>
+        <div class="d-flex">
+            <svg height="40" width="40" xmlns="http://www.w3.org/2000/svg" style="margin-top: 4px;">
+                <circle r="12" cx="12" cy="12" fill="#c5eede" />
+                Sorry, your browser does not support inline SVG.
+            </svg>
+            <p class="para ps-2">Free media you can use any where</p>
+        </div>
+        <div class="ms-5">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt ipsa perferendis asperiores dicta
+                nemo consequuntur. Praesentium officia ducimus.</p>
+            <a class="a" href="#">Learn about our license</a>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h3>Pixabay</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore suscipit corporis officiis ad.
+                    </p>
+                </div>
+                <div class="col-2">
+                    <ul style="list-style: none;">
+                        <li><h5>Discover</h5></li>
+                        <li>skfk</li>
+                        <li>fklefnpo</li>
+                        <li>rf,gnw</li>
+                        <li>rfpoer</li>
+                        <li>poreo</li>
+                        <li>ror4o</li>
+                    </ul>
+                </div>
+                <div class="col-2">
+                    <ul style="list-style: none;">
+                        <li><h5>Community</h5></li>
+                        <li>skfk</li>
+                        <li>fklefnpo</li>
+                        <li>rf,gnw</li>
+                        <li>rfpoer</li>
+                        <li>poreo</li>
+                        <li>ror4o</li>
+                    </ul>
+                </div>
+                <div class="col-2">
+                    <ul style="list-style: none;">
+                        <li><h5>About</h5></li>
+                        <li>skfk</li>
+                        <li>fklefnpo</li>
+                        <li>rf,gnw</li>
+                        <li>rfpoer</li>
+                        <li>poreo</li>
+                        <li>ror4o</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
