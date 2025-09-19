9e30dcfa655060c8979b3fce680a5b8338da4a9b
ammaz-awan <ammazawan@gmail.com>
2025-09-19 09:04:01 -0700

    changes

--- views/images/create.php
+++ views/images/create.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "../../../views/layout/nav.php";
 require_once __DIR__. "../../../views/layout/sidebar.php";
 ?>
 
-S
+
   <section class="add-image-form">
     <h2>Add New Image</h2>
     <form action="../../App/RequestManager/RequestManager.php" id="addImageForm" enctype="multipart/form-data">
